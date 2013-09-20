5ac18452c3135fde621eadafc4c84767511041e7
Marshsall Klickman <mklickman1@gmail.com>
2013-09-20 13:32:05 -0400

    Responsive CSS for new Front End Engineering page

--- source/javascripts/front-end-2.js
+++ source/javascripts/front-end-2.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  
+  // FAQ open/close behavior
+  //------------------------------------------------------------------------------------------------//
+  
   $('li.question.open').removeClass('open');
 
   var $questions = $('.faq-questions li.question');
@@ -11,4 +15,12 @@ $(document).ready(function() {
 
     $(this).parents('.question').addClass('open');
   });
+
+
+  // Flowtype.js
+  // http://simplefocus.com/flowtype
+  //------------------------------------------------------------------------------------------------//
+
+  $('.fed-2 h2').fitText(1.15, {maxFontSize: '35px'});
+
 });
